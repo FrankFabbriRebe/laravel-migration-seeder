f44f04968a9a9a335e5024b60cf5e8fc673b1bfa
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-13 16:49:42 +0100

    create: TrainFactory

---
+++ database/factories/TrainFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Train>
+ */
+class TrainFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'azienda' => fake()->company,
+            'stazione_di_partenza' => fake()->city,
+            'stazione_di_arrivo' => fake()->city,
+            'orario_di_partenza' => fake()->time,
+            'orario_di_arrivo' => fake()->time,
+            'codice_treno' => fake()->numberBetween(10000, 99999),
+            'numero_carrozze' => fake()->randomDigitNotNull,
+            'in_orario' => fake()->boolean(),
+            'cancellato' => fake()->boolean(),
+        ];
+    }
+}
