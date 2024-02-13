1faa4e06aaae3816db4109b07142d238b819d35e
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-13 17:42:50 +0100

    add: content

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,4 +4,21 @@
 @endsection
 @section('content')
 <h1>Table train</h1>
+<ul>
+    @foreach ($trains as $train)
+
+    <li>Azienda: {{$train -> azienda}}</li>
+    <li>Stazione di patenza: {{$train -> stazione_di_partenza}}</li>
+    <li>Stazione di arrivo: {{$train -> stazione_di_arrivo}}</li>
+    <li>Orario di partenza: {{$train -> orario_di_partenza}}</li>
+    <li>Orario di arrivo: {{$train -> orario_di_arrivo}}</li>
+    <li>Codice treno: {{$train -> codice_treno}}</li>
+    <li>Numero numero carrozze: {{$train -> numero_carrozze}}</li>
+    <li>In orario: {{$train -> in_orario}}</li>
+    <li>Cancellato: {{$train -> cancellato}}</li>
+
+    <br>
+
+    @endforeach
+</ul>
 @endsection
