bfc68296638668230853c3ac7f444e0a901cf0a4
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-13 16:22:07 +0100

    create: cols

--- database/migrations/2024_02_13_151112_create_trains_table.php
+++ database/migrations/2024_02_13_151112_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno')->unique();
+            $table->unsignedInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+
             $table->timestamps();
         });
     }
