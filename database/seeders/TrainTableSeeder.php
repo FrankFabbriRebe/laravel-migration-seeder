d278c3d2efbff954a8c62ea476cb20cfa71ca12c
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-13 17:19:39 +0100

    import: Train model

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// import train model
+use App\Models\Train;
+
 class TrainTableSeeder extends Seeder
 {
     /**
